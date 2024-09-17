d33006e0648747e9c61b1b6538e83cfed3650696
SeoHyeon <qortj1120@kookmin.ac.kr>
2024-09-18 01:03:53 +0900

    test

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
+
 const mysql = require('mysql2');
 
 // MySQL 데이터베이스 연결 설정
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',      // MySQL 사용자 이름
-  password: '0818',      // MySQL 비밀번호
-  database: 'character_new',
+  password: 'seohyun11@',      // MySQL 비밀번호
+  database: 'test_db',
   connectTimeout : 100000,
 });
 
@@ -15,7 +16,7 @@ db.connect((err) => {
     console.error('MySQL 연결 실패: ', err);
     return;
   }
-  console.log('characternew에 성공적으로 연결되었습니다.');
+  console.log('db에 성공적으로 연결되었습니다.');
 });
 
 
