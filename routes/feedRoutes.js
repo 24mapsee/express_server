d1cd25c9850ca1dbafa5ecd6327901007d85ab8b
TaeYoon <tyk117@kookmin.ac.kr>
2024-10-28 20:28:42 +0900

    Fix: location err

--- routes/feedRoutes.js
+++ routes/feedRoutes.js
@@ -1,12 +1,12 @@
 // routes/feedRoutes.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const feedController = require('../controllers/feedController');
+const feedController = require("../controllers/feedControllers");
 
 // Get all shared routes for the feed
-router.get('/', feedController.getFeed);
+router.get("/", feedController.getFeed);
 
 // Share a route to the feed
-router.post('/share', feedController.shareRoute);
+router.post("/share", feedController.shareToFeed);
 
 module.exports = router;
