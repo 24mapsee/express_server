4c6e85327e83c43649162300f3d5f399f0ae37a6
eunseong <klaqwe1@naver.com>
2024-10-30 16:14:43 +0900

    Fix: modify

--- controllers/modifyInfoController.js
+++ controllers/modifyInfoController.js
@@ -3,7 +3,7 @@ const db = require('../models/db');  // 데이터베이스 연결 파일
 // 사용자 정보 수정 함수
 exports.modifyUserInfo = async (req, res) => {
     const { userId } = req.params; // URL 파라미터에서 userId 가져오기
-    const { name, phone_number, birth_date, gender } = req.body; // 수정할 필드들 가져오기
+    const { name, phone_number, birth_date, bio,email } = req.body; // 수정할 필드들 가져오기
     try {
         // 사용자 정보 업데이트
         const [result] = await db.query(`
@@ -13,10 +13,11 @@ exports.modifyUserInfo = async (req, res) => {
                 phone_number = ?,
                 birth_date = ?,
                 bio = ?,
+                email = ?,
                 updated_at = CURRENT_TIMESTAMP
             WHERE 
                 user_id = ?
-        `, [name, phone_number, birth_date, gender, userId]);
+        `, [name, phone_number, birth_date, bio,email, userId]);
 
         // 업데이트 성공 여부 확인
         if (result.affectedRows === 0) {
