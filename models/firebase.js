cd8cb24f65bc7f12f0415ee27afc5519f3f3b266
eunseong <klaqwe1@naver.com>
2024-10-30 22:50:22 +0900

    Fix: firebase.js

--- models/firebase.js
+++ models/firebase.js
@@ -1,14 +1,31 @@
-const admin = require("firebase-admin");
-const serviceAccountPath =
-  process.env.NODE_ENV === "production"
-    ? "/config/serviceAccountKey"
-    : "../mapsee-8a424-firebase-adminsdk-cys83-b267c9438a.json";
-const serviceAccount = require(serviceAccountPath);
+// const admin = require("firebase-admin");
+// const serviceAccountPath =
+//   process.env.NODE_ENV === "production"
+//     ? "/config/serviceAccountKey"
+//     : "../mapsee-8a424-firebase-adminsdk-cys83-b267c9438a.json";
+// const serviceAccount = require(serviceAccountPath);
 
-// Firebase Admin SDK 초기화
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://mapsee-8a424.firebaseio.com",
-});
+// // Firebase Admin SDK 초기화
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://mapsee-8a424.firebaseio.com",
+// });
+
+// module.exports = admin;
+
+const admin = require("firebase-admin");
+
+// Firebase 설정 파일 로드를 주석 처리합니다.
+// const serviceAccountPath =
+//   process.env.NODE_ENV === "production"
+//     ? "/config/serviceAccountKey"
+//     : "../mapsee-8a424-firebase-adminsdk-cys83-b267c9438a.json";
+// const serviceAccount = require(serviceAccountPath);
+
+// Firebase Admin SDK 초기화를 주석 처리합니다.
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://mapsee-8a424.firebaseio.com",
+// });
 
 module.exports = admin;
