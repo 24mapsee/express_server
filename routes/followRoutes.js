648175dfdfd0ed117f9795c679f5a00033122f24
eunseong <klaqwe1@naver.com>
2024-10-31 12:37:31 +0900

    fix: follow

--- routes/followRoutes.js
+++ routes/followRoutes.js
@@ -3,8 +3,8 @@ const followController = require("../controllers/followController");
 
 const router = express.Router();
 
-router.get("/follower/:userId", followController.getFollowers);
-router.get("/following/:userId", followController.getFollowing);
+router.get("/follower/:userId", followController.getFollowing);
+router.get("/following/:userId", followController.getFollowers);
 
 router.post("/add-follow", followController.addFollow);
 router.delete("/delete-follow", followController.deleteFollow); 
