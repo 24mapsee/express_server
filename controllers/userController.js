df7ccfc1282b1dd3c6785ac991804bcbbee91555
TaeYoon <tyk117@kookmin.ac.kr>
2024-11-01 14:33:36 +0900

    Fix: google login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,110 +1,125 @@
-const db = require('../models/db'); // MySQL 데이터베이스
-const admin = require('../models/firebase'); // Firebase Admin SDK
-const bcrypt = require('bcrypt');
+const db = require("../models/db"); // MySQL 데이터베이스
+const admin = require("../models/firebase"); // Firebase Admin SDK
+const bcrypt = require("bcrypt");
 const axios = require("axios");
 
 // 디폴트 폴더 생성 함수
 const createDefaultFolder = async (userId, connection) => {
-    try {
-        await connection.execute(
-            "INSERT INTO Place_Folders (user_id, folder_name, created_at) VALUES (?, ?, CURRENT_TIMESTAMP)",
-            [userId, '내 저장소']
-        );
-    } catch (error) {
-        throw new Error('Error creating default folder: ' + error.message);
-    }
+  try {
+    await connection.execute(
+      "INSERT INTO Place_Folders (user_id, folder_name, created_at) VALUES (?, ?, CURRENT_TIMESTAMP)",
+      [userId, "내 저장소"]
+    );
+  } catch (error) {
+    throw new Error("Error creating default folder: " + error.message);
+  }
 };
 
 // 1. 자체 회원가입 (아이디/비밀번호)
 exports.registerNative = async (req, res) => {
-    const { userId, password } = req.body; // 클라이언트에서 제공한 사용자 ID와 비밀번호
-    const connection = await db.getConnection();
+  const { userId, password } = req.body; // 클라이언트에서 제공한 사용자 ID와 비밀번호
+  const connection = await db.getConnection();
 
-    try {
-         // 기존 userId 중복 확인
-         const [existingUser] = await connection.execute(
-            "SELECT * FROM Users WHERE local_id = ?",
-            [userId]
-        );
+  try {
+    // 기존 userId 중복 확인
+    const [existingUser] = await connection.execute(
+      "SELECT * FROM Users WHERE local_id = ?",
+      [userId]
+    );
 
-        if (existingUser.length > 0) {
-            // 중복된 아이디 에러 처리
-            return res.status(409).json({ message: "중복된 아이디입니다" });
-        }
-        // 비밀번호 암호화
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Firebase에 사용자 생성
-        const userRecord = await admin.auth().createUser({
-            password: password // Firebase에서 고유 UID를 생성하기 위한 비밀번호
-        });
-
-        // MySQL Users 테이블에 유저 정보 저장
-        await connection.execute(
-            "INSERT INTO Users (user_id, local_id, password, name, created_at) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP)",
-            [userRecord.uid, userId, hashedPassword, userId]
-        );
-
-        // 기본 폴더 생성
-        await createDefaultFolder(userRecord.uid, connection);
-
-        res.status(200).json({
-            message: 'User registered successfully',
-            uid: userRecord.uid
-        });
-    } catch (error) {
-        console.error('Error registering user:', error);
-        res.status(500).json({ message: 'Error registering user', error: error.message });
-    } finally {
-        connection.release();
+    if (existingUser.length > 0) {
+      // 중복된 아이디 에러 처리
+      return res.status(409).json({ message: "중복된 아이디입니다" });
     }
+    // 비밀번호 암호화
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Firebase에 사용자 생성
+    const userRecord = await admin.auth().createUser({
+      password: password, // Firebase에서 고유 UID를 생성하기 위한 비밀번호
+    });
+
+    // MySQL Users 테이블에 유저 정보 저장
+    await connection.execute(
+      "INSERT INTO Users (user_id, local_id, password, name, created_at) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP)",
+      [userRecord.uid, userId, hashedPassword, userId]
+    );
+
+    // 기본 폴더 생성
+    await createDefaultFolder(userRecord.uid, connection);
+
+    res.status(200).json({
+      message: "User registered successfully",
+      uid: userRecord.uid,
+    });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: error.message });
+  } finally {
+    connection.release();
+  }
 };
 
 // 2. 소셜 회원가입
 exports.registerWithSocial = async (req, res) => {
-    const { idToken, accessToken, provider } = req.body;
-    let uid, email;
-    const connection = await db.getConnection();
+  const { idToken, accessToken, uid, provider } = req.body;
+  let email, name, profileImage;
+  const connection = await db.getConnection();
 
-    try {
-        if (provider === 'google') {
-            // Google 인증 처리
-            const decodedToken = await admin.auth().verifyIdToken(idToken);
-            uid = decodedToken.uid;
-            email = decodedToken.email;
-        } else if (provider === 'kakao') {
-            // Kakao 인증 처리
-            const kakaoUserInfo = await axios.get("https://kapi.kakao.com/v2/user/me", {
-                headers: { Authorization: `Bearer ${accessToken}` },
-            });
-            uid = `kakao_${kakaoUserInfo.data.id}`;
-            email = kakaoUserInfo.data.kakao_account.email;
-        } else if (provider === 'naver') {
-            // Naver 인증 처리
-            const naverUserInfo = await axios.get("https://openapi.naver.com/v1/nid/me", {
-                headers: { Authorization: `Bearer ${accessToken}` },
-            });
-            uid = `naver_${naverUserInfo.data.response.id}`;
-            email = naverUserInfo.data.response.email;
+  try {
+    if (provider === "google") {
+      // Google 인증 처리
+      // const decodedToken = await admin.auth().verifyIdToken(idToken);
+      const userRecord = await admin.auth().getUser(uid);
+      email = userRecord.email;
+      name = userRecord.displayName;
+      profileImage = userRecord.photoURL;
+    } else if (provider === "kakao") {
+      // Kakao 인증 처리
+      const kakaoUserInfo = await axios.get(
+        "https://kapi.kakao.com/v2/user/me",
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
         }
-
-        // Firebase Custom Token 생성
-        const customToken = await admin.auth().createCustomToken(uid);
-
-        // 데이터베이스에 사용자 정보 저장
-        await connection.execute(
-            "INSERT INTO Users (user_id, email, provider, created_at) VALUES (?, ?, ?, CURRENT_TIMESTAMP) ON DUPLICATE KEY UPDATE email = VALUES(email), provider = VALUES(provider)",
-            [uid, email, provider]
-        );
-
-        // 기본 폴더 생성
-        await createDefaultFolder(uid, connection);
-
-        res.status(200).json({ message: `${provider} user authenticated`, customToken });
-    } catch (error) {
-        console.error(`Error authenticating ${provider} user:`, error);
-        res.status(500).json({ message: `Error authenticating ${provider} user`, error: error.message });
-    } finally {
-        connection.release(); // 연결 해제
+      );
+      uid = `kakao_${kakaoUserInfo.data.id}`;
+      email = kakaoUserInfo.data.kakao_account.email;
+    } else if (provider === "naver") {
+      // Naver 인증 처리
+      const naverUserInfo = await axios.get(
+        "https://openapi.naver.com/v1/nid/me",
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        }
+      );
+      uid = `naver_${naverUserInfo.data.response.id}`;
+      email = naverUserInfo.data.response.email;
     }
+
+    // Firebase Custom Token 생성
+    const customToken = await admin.auth().createCustomToken(uid);
+
+    // 데이터베이스에 사용자 정보 저장
+    await connection.execute(
+      "INSERT INTO Users (user_id, email, provider, profile_picture, created_at) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP) ON DUPLICATE KEY UPDATE email = VALUES(email), provider = VALUES(provider), profile_picture = VALUES(profile_picture);",
+      [uid, email, profileImage, provider]
+    );
+
+    // 기본 폴더 생성
+    await createDefaultFolder(uid, connection);
+
+    res
+      .status(200)
+      .json({ message: `${provider} user authenticated`, customToken });
+  } catch (error) {
+    console.error(`Error authenticating ${provider} user:`, error);
+    res.status(500).json({
+      message: `Error authenticating ${provider} user`,
+      error: error.message,
+    });
+  } finally {
+    connection.release(); // 연결 해제
+  }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,8 +8,10 @@ const router = express.Router();
 router.post(
   "/register/native",
   [
-      body("userId").notEmpty().withMessage("아이디를 입력해주세요."),
-      body("password").isLength({ min: 6 }).withMessage("비밀번호는 최소 6자리 이상이어야 합니다.")
+    body("userId").notEmpty().withMessage("아이디를 입력해주세요."),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("비밀번호는 최소 6자리 이상이어야 합니다."),
   ],
   userController.registerNative
 );
@@ -18,26 +20,39 @@ router.post(
 router.post(
   "/register/social",
   [
-      body("idToken").notEmpty().withMessage("소셜 토큰을 입력해주세요."),
-      body("provider").isIn(['google', 'kakao', 'naver']).withMessage("유효하지 않은 소셜 제공자입니다.")
+    body("provider")
+      .isIn(["google", "kakao", "naver"])
+      .withMessage("유효하지 않은 소셜 제공자입니다."),
   ],
   userController.registerWithSocial
 );
 
 // 소셜 로그인 엔드포인트
-router.post("/auth/google", (req, res, next) => {
-  req.body.provider = 'google';
-  next();
-}, userController.registerWithSocial);
+router.post(
+  "/auth/google",
+  (req, res, next) => {
+    req.body.provider = "google";
+    next();
+  },
+  userController.registerWithSocial
+);
 
-router.post("/auth/kakao", (req, res, next) => {
-  req.body.provider = 'kakao';
-  next();
-}, userController.registerWithSocial);
+router.post(
+  "/auth/kakao",
+  (req, res, next) => {
+    req.body.provider = "kakao";
+    next();
+  },
+  userController.registerWithSocial
+);
 
-router.post("/auth/naver", (req, res, next) => {
-  req.body.provider = 'naver';
-  next();
-}, userController.registerWithSocial);
+router.post(
+  "/auth/naver",
+  (req, res, next) => {
+    req.body.provider = "naver";
+    next();
+  },
+  userController.registerWithSocial
+);
 
 module.exports = router;
